f897e24c785fc78e9511f7fbda5af3b7cf26887a
Peter Sideris <petersid2022@gmail.com>
2024-09-26 14:20:26 +0300

    themes and such

--- config.h
+++ config.h
@@ -14,26 +14,34 @@ static const char *mednextcmd[] = {"playerctl", "next", NULL};
 static const char *medprevcmd[] = {"playerctl", "previous", NULL};
 
 /* appearance */
+static const int topbar = 1;            /* 0 means bottom bar */
+static const int showbar = 1;           /* 0 means no bar */
+static const int showsystray = 1;        /* 0 means no systray */
+static const unsigned int snap = 10;    /* snap pixel */
 static const unsigned int borderpx = 1; /* border pixel of windows */
-static const unsigned int snap = 32;    /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar = 1;           /* 0 means no bar */
-static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
+static const unsigned int systrayspacing = 20;   /* systray spacing */
+static const char *fonts[] = {"monospace:size=13"};
 static const char dmenufont[] = "monospace:size=10";
-static const char col_gray1[] = "#222222";
-static const char col_gray2[] = "#444444";
-static const char col_gray3[] = "#bbbbbb";
-static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#005577";
-static const char *colors[][3] = {
-    /*               fg         bg         border   */
-    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-    [SchemeSel] = {col_gray4, col_cyan, col_cyan},
+
+static const char norm_fg[] = "#f2e4df";
+static const char norm_bg[] = "#0f1011";
+static const char norm_border[] = "#a99f9c";
+
+static const char sel_fg[] = "#f2e4df";
+static const char sel_bg[] = "#E02406";
+static const char sel_border[] = "#f2e4df";
+
+static const char urg_fg[] = "#f2e4df";
+static const char urg_bg[] = "#CF0D03";
+static const char urg_border[] = "#CF0D03";
+
+static const char *colors[][3]      = {
+    /*               fg           bg         border                         */
+    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
+    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
 };
 
 /* tagging */
@@ -119,18 +127,8 @@ static const Key keys[] = {
     TAGKEYS(XK_9, 8)
 };
 
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+  {ClkTagBar, 0, Button1, view, {0}},
+  {ClkClientWin, MODKEY, Button1, movemouse, {0}},
+  {ClkClientWin, MODKEY | ShiftMask, Button1, resizemouse, {0}},
 };
